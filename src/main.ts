2f6c11d2e1dd5fc6bd3eb8be1fce3927de7fe171
liora <liorahron11@gmail.com>
2024-11-15 12:01:39 +0200

    feat(): add middlewares

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,14 @@
-import express, {Express} from 'express';
 require('dotenv').config()
+import express, {Express} from 'express';
+import cors from 'cors';
 
 const app: Express = express();
-const port: number = Number(process.env.port);
+app.use(express.json());
+app.use(cors());
 
 const runApp = (app: Express) => {
+    const port: number = Number(process.env.port);
+
     app.listen(port, () => {
         return console.log(`app is running at port ${port}`);
     });
